aa0ec4f251140c9bc049d6f803beb593628c3dd0
ronaldo-lourenco <ronaldo.lourenco@escola.pr.gov.br>
2024-10-26 00:43:55 +0000

    Atualização config

--- config.php
+++ config.php
@@ -3,6 +3,11 @@ protegeArquivo(basename(__FILE__)) ;
 
 $incluiu_estilo = FALSE ;
 
+$DB_HOST = $_ENV("DB_HOST") ;
+$DB_USER = $_ENV("DB_USER") ;
+$DB_PASSWORD = $_ENV("DB_PASSWORD") ;
+$DB_NAME = $_ENV("DB_NAME") ;
+$DB_PORT = $_ENV("DB_PORT") ;
 define("MASKCPF", "999.999.999-99") ;
 define("MASKCNPJ", "99.999.999/9999-99") ;
 define("MASKFONE", "(99) 9999-9999") ;
@@ -22,7 +27,7 @@ define("COR_LINHA_PAR_TABELA", '#E2E4FF') ;
 define("INDEXPHP", "painel.php") ;
 define("BASEPATH", dirname(__FILE__)."/") ;
 //define("BASEURL", "http://localhost/paineladm/") ;
-define("BASEURL", "http://192.168.0.6/tofas/") ;
+define("BASEURL", $DB_HOST) ;
 define("ADMURL", BASEURL."painel.php") ;
 define("TEMP", BASEPATH."temp/") ;
 define("BLOB", BASEPATH."blob/") ;
@@ -33,10 +38,10 @@ define("CSSPATH", BASEURL) ;
 define("JSPATH", BASEURL."js/") ;
 
 //banco de dados
-define("DBHOST", "localhost") ;
+define("DBHOST", $DB_HOST) ;
 //define("DBHOST", "http://192.168.0.6") ;
-define("DBUSER", "root") ;
-define("DBPASS", "root") ;
-define("DBNAME", "railway") ;
+define("DBUSER", $DB_USER) ;
+define("DBPASS", $DB_PASSWORD) ;
+define("DBNAME", $DB_NAME) ;
 define("DBMENU", "configmenu") ;
 ?>
